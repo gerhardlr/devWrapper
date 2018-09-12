d043bc512444d4ebf6e105fdaa73f69632118f4c
gerhardlr <gerhard@ska.ac.za>
2018-09-12 10:58:13 +0200

    added dos envirmental var handling

--- runDev.bat
+++ runDev.bat
@@ -1,8 +1,8 @@
 @echo off
 cd dev/
-set DEV=$(cd)
+set DEV=%cd%
 cd ..
 cd client/
-set CLIENT=$(cd)
+set CLIENT=%cd%
 cd ..
-docker run -it --entrypoint /bin/bash --rm  --name=flaskio -p 127.0.0.1:5003:5000 -p 127.0.0.1:3000:3000 --network=docker-network -v $DEV:/app -v $CLIENT:/client flaskio
+docker run -it --entrypoint /bin/bash --rm  --name=flaskio -p 127.0.0.1:5003:5000 -p 127.0.0.1:3000:3000 --network=docker-network -v %DEV%:/app -v %CLIENT%:/client flaskio
