0158087a749a37f363fea75d0a8cc019fddf597d
gerhardlr <gerhard@ska.ac.za>
2018-09-12 16:23:47 +0200

    m

--- runDev.bat
+++ runDev.bat
@@ -5,4 +5,4 @@ cd ..
 cd client/
 set CLIENT=%cd%
 cd ..
-docker run -it --entrypoint /bin/bash --rm  --name=flaskio -p 127.0.0.1:5003:5000 -p 127.0.0.1:3000:3000 --network=docker-network -v %DEV%:/app -v %CLIENT%:/client flaskio
+docker run -it --entrypoint /bin/bash --rm  --name=flaskio -p 127.0.0.1:5003:5000 -p 127.0.0.1:3000:3000 --network=docker-network -v "%DEV%":/app -v "%CLIENT%":/client flaskio
