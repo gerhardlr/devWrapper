7735d6361ce15725e4407a50d6c3d1e5c4c575b1
gerhardlr <gerhard@ska.ac.za>
2018-09-13 12:45:40 +0200

    added fixtures setup/down bat2

--- Fixtures/setdown.bat
+++ Fixtures/setdown.bat
@@ -1,4 +1,4 @@
 @echo off
-docker-compose -f docker-compose.tango_gui.yml down --remove-orphans
-docker-compose -f docker-compose.tangobase.yml down --remove-orphans
+docker-compose -f docker-compose.tango_gui.yml down
+docker-compose -f docker-compose.tangobase.yml down
 docker network remove docker-network
--- Fixtures/setdown.sh
+++ Fixtures/setdown.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.tango_gui.yml down --remove-orphans
-docker-compose -f docker-compose.tangobase.yml down --remove-orphans
+docker-compose -f docker-compose.tango_gui.yml down
+docker-compose -f docker-compose.tangobase.yml down
 docker network remove docker-network
