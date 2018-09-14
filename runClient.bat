ac88006366a02b013103960388fe0ff8132b285b
gerhardlr <gerhard@ska.ac.za>
2018-09-14 11:41:49 +0200

    seperate client from server4

--- runClient.bat
+++ runClient.bat
@@ -1,3 +1,3 @@
 echo off
 cd client/dev-client/
-docker run -it --entrypoint /bin/bash --rm  --name=dev_client -p 127.0.0.1:3000:3000 --network=docker-network -v "%(pwd)%":/dev-client dev_client
+docker run -it --entrypoint /bin/bash --rm  --name=dev_client -p 127.0.0.1:3000:3000 --network=docker-network -v "%pwd%":/dev-client dev_client
